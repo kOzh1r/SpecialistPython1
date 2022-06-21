0578179495aed394febf8a0cd63177329f9c6dd3
kOzh1r <106873916+kOzh1r@users.noreply.github.com>
2022-06-21 21:07:31 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -5,9 +5,25 @@
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    AB = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    return AB
 
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+x1 = int(input("Координаты точки А. х = "))
+y1 = int(input("Координаты точки А. у = "))
+x2 = int(input("Координаты точки В. х = "))
+y2 = int(input("Координаты точки В. у = "))
+x3 = int(input("Координаты точки С. х = "))
+y3 = int(input("Координаты точки С. у = "))
+
+if distance(x1, y1, x2, y2) <= distance(x2, y2, x3, y3) and distance(x1, y1, x2, y2) <= distance(x1, y1, x3, y3):
+    total = distance(x1, y1, x2, y2)
+    lenght = 'AB'
+elif distance(x1, y1, x2, y2) >= distance(x2, y2, x3, y3) and distance(x2, y2, x3, y3) <= distance(x1, y1, x3, y3):
+    total = distance(x2, y2, x3, y3)
+    lenght = 'BC'
+else:
+    total = distance(x1, y1, x3, y3)
+    lenght = 'AC'
+
+print(f"Самый короткий отрезок: {lenght} - {round(total, 2)}")
